9a629ba16cee80ad9233bb010fe5f8c346538d74
linh050297 <linh050297@gmail.com>
2021-11-24 07:43:46 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+// import logo from "./logo.svg";
+import { CardList } from "./components/card-list/card-list.component";
+import "./App.css";
+import { SearchBox } from "./components/search-box/search-box.component";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monstersArray: [],
+      searchString: "",
+    };
+
+    // this.handleChange = this.handleChange.bind(this);
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((res) => {
+        this.setState({ monstersArray: res });
+      });
+  }
+
+  // arrow function tự động bind this thành context của component ( nơi function handleChange được khởi tạo )
+  handleChange = (e) => {
+    this.setState({ searchString: e.target.value });
+  };
+
+  render() {
+    const { monstersArray, searchString } = this.state;
+    const filteredMonster = monstersArray.filter((item) =>
+      item.name.toLowerCase().includes(searchString.toLowerCase())
+    );
+
+    return (
+      <div>
+        <div className="App">
+          <h1>Monster Monsterdex</h1>
+          <SearchBox
+            placeholder="Please type to search !!!"
+            handleChange={this.handleChange}
+          />
+          <CardList monsters={filteredMonster}></CardList>
+        </div>
+        <button onClick={() => this.setState({ monstersArray: [] })}>
+          Clear
+        </button>
+      </div>
+    );
+  }
 }
 
 export default App;
